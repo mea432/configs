ce2d6b51727641ef74671a3f4768902042250349
mea432 <easonn999@gmail.com>
2026-05-31 14:48:28 -0400

    add unfocused populated aerospace workspaces to sketchybar

---
+++ sketchybar/plugins/aerospace_workspaces.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#!/bin/bash
+
+# Get a list of all non-empty workspaces on the focused monitor
+POPULATED_ON_FOCUSED_MONITOR=$(aerospace list-workspaces --monitor focused --empty no)
+
+# Get the currently focused workspace name
+FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
+
+# Filter out the focused workspace from the list of populated ones on the focused monitor
+UNFOCUSED_POPULATED=$(echo "$POPULATED_ON_FOCUSED_MONITOR" | grep -v -F -x "$FOCUSED_WORKSPACE")
+
+# Format the list for sketchybar
+FORMATTED_WORKSPACES=$(echo "$UNFOCUSED_POPULATED" | tr '\n' ' ')
+
+sketchybar --set $NAME label="$FORMATTED_WORKSPACES"
