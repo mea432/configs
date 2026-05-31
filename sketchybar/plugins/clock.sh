74de64ee6ebb49a5cd8f843894901b85218e58bd
mea432 <easonn999@gmail.com>
2026-05-31 13:57:08 -0400

    make clock 5 minutes ahead so hopefully I don't miss any deadlines

--- sketchybar/plugins/clock.sh
+++ sketchybar/plugins/clock.sh
@@ -3,4 +3,4 @@
 # The $NAME variable is passed from sketchybar and holds the name of
 # the item invoking this script:
 
-sketchybar --set "$NAME" label="$(date +'%a %b %d %-I:%M %p')" padding_left=0
+sketchybar --set "$NAME" label="$(date -v+5M +'%a %b %d %-I:%M %p')" padding_left=0
