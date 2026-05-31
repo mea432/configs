b873ae6da1916e74765f3aa7bb50504d66953a78
mea432 <easonn999@gmail.com>
2026-05-31 00:46:36 -0400

    add aerospace fullscreen status to sketchybar

--- sketchybar/plugins/aerospace_window_title.sh
+++ sketchybar/plugins/aerospace_window_title.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-title=$(aerospace list-workspaces --focused)
+# Get the name of the focused workspace first
+name=$(/opt/homebrew/bin/aerospace list-workspaces --focused)
 
-# Fallback if no window is focused
-if [ -z "$title" ]; then
-  title="No Window"
+# Check if any window in the focused workspace is fullscreen
+is_fullscreen=$((/opt/homebrew/bin/aerospace list-windows --workspace focused --format '%{window-is-fullscreen}' | grep -q 'true' && echo "true") || echo "false")
+
+# Fallback if no workspace is focused
+if [ -z "$name" ]; then
+  name="No Window"
 fi
 
-sketchybar --set window_title label="$title"
+# Add an asterisk if the workspace is fullscreen
+if [ "$is_fullscreen" = "true" ]; then
+  name="* $name"
+fi
+
+sketchybar --set window_title label="$name"
