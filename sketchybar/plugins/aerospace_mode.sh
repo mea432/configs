9b9203c2527b02b46304ff9419e829c824bb46a2
mea432 <easonn999@gmail.com>
2026-05-31 00:29:45 -0400

    add aerospace mode to sketchybar

--- sketchybar/plugins/aerospace_mode.sh
+++ sketchybar/plugins/aerospace_mode.sh
@@ -2,10 +2,8 @@
 
 CURRENT_MODE=$(aerospace list-modes --current)
 
-if [ "$CURRENT_MODE" == "main" ]; then
-  sketchybar --bar color=0x66000000 --set "$NAME" drawing=off
-elif [ "$CURRENT_MODE" == "service" ]; then
-  sketchybar --bar color=0x66cc5555 --set "$NAME" drawing=on
+if [ "$CURRENT_MODE" = "main" ]; then
+  sketchybar --set "$NAME" drawing=off
 else
-  sketchybar --bar color=0x6600ffaa --set "$NAME" drawing=on
+  sketchybar --set "$NAME" drawing=on label="$CURRENT_MODE"
 fi
