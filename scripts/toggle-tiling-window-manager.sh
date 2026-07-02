12ec1d688374309a0125c37bc1c14745d51b319f
mea432 <easonn999@gmail.com>
2026-07-01 23:46:34 -0400

    correct some stuff; remove jankyborders

--- scripts/toggle-tiling-window-manager.sh
+++ scripts/toggle-tiling-window-manager.sh
@@ -11,16 +11,9 @@
 # Documentation:
 # @raycast.description Toggle Aerospace, Jankyborders, and Sketchybar
 
-STATE_FILE="/tmp/aerospace_enabled_flag"
+STATE_FILE="/tmp/aerospace_disabled_flag"
 
 if [ -f "$STATE_FILE" ]; then
-  # --- TURN EVERYTHING OFF ---
-  aerospace enable off
-  pkill -x borders
-  pkill -x sketchybar
-  rm "$STATE_FILE"
-  echo "AeroSpace & Borders: OFF"
-else
   # --- TURN EVERYTHING ON ---
   aerospace enable on
 
@@ -30,9 +23,16 @@ else
 
   # Start borders (adjust path if 'borders' isn't in your $PATH)
   # borders active_color=0xffeb212e inactive_color=0x00000000 width=5.0 &
-  borders &
+  # borders &
   sketchybar &
 
+  rm "$STATE_FILE"
+  echo "AeroSpace & Sketchybar: ON"
+else
+  # --- TURN EVERYTHING OFF ---
+  aerospace enable off
+  # pkill -x borders
+  pkill -x sketchybar
   touch "$STATE_FILE"
-  echo "AeroSpace & Borders: ON"
+  echo "AeroSpace & Sketchybar: OFF"
 fi
